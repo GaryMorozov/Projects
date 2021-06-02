8a4bb3c0db84409b6e95c09cc563197e32739f9e
garymorozov <garymorozov@gmail.com>
2021-06-02 15:54:32 +0300

    исправление ошибки в index.php

--- k8s/index.php
+++ k8s/index.php
@@ -4,4 +4,3 @@ $ip_server = $_SERVER['SERVER_ADDR'];
 echo "<h1> Hello feom K8s!</h1><br>";
 echo "Server IP-address is $ip_server","<br><cp>";
 echo "Made by <font color=blue>Igor Morozov for Denis Astahov!";
-</php>
